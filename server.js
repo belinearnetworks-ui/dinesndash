172496fcb26425ebe1ab956957ac3d1df8f96d78
belinearnetworks-ui <belinearnetworks@gmail.com>
2026-02-13 11:11:31 +0000

    New  Updates

--- server.js
+++ server.js
@@ -167,10 +167,10 @@ function driver(req,res,next){
 }
 
 let con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     port: process.env.DB_PORT || 3306
 });
 
