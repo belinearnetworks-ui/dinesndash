4a2726ece4108c107b363ca57ca052946b656362
belinearnetworks-ui <belinearnetworks@gmail.com>
2026-02-13 10:56:58 +0000

    New Massive Updates

--- server.js
+++ server.js
@@ -166,17 +166,14 @@ function driver(req,res,next){
   next();
 }
 
-let con=mysql.createConnection({
-    user:'Sizwe',
-    password:'Trigonometry',
-    host:"localhost",
-    database:"dinesndash"
-})
+let con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+});
 
-con.connect(err=>{
-    if(err) throw err;
-    console.log("Mysql Connected")
-})
 
 app.get("/",authenticated,(req,res)=>{
     res.render("home")
